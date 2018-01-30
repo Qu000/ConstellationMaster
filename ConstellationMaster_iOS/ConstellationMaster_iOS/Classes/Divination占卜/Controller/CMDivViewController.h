95aa05438fc5ed929d8644dd2f0706bc0d220a69
jiahong <qu_jia_hong@163.com>
2018-01-30 15:15:45 +0800

    something wrong

---
+++ ConstellationMaster_iOS/ConstellationMaster_iOS/Classes/Divination占卜/Controller/CMDivViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMDivViewController.h
+//  ConstellationMaster_iOS
+//
+//  Created by qujiahong on 2018/1/30.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import "CMBaseViewController.h"
+
+@interface CMDivViewController : CMBaseViewController
+
+@end
---
+++ ConstellationMaster_iOS/ConstellationMaster_iOS/Classes/Home首页/Controller/CMHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMHomeViewController.h
+//  ConstellationMaster_iOS
+//
+//  Created by qujiahong on 2018/1/30.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMHomeViewController : UIViewController
+
+@end
