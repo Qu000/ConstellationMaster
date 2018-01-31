654cdd2df05175c6b4d3880bf2654b4740ab9e61
jiahong <qu_jia_hong@163.com>
2018-01-31 17:05:46 +0800

    封装网络层，抓取接口

---
+++ ConstellationMaster_iOS/ConstellationMaster_iOS/Classes/Friends好友/View/CMFriRemindView.h
@@ -0,0 +1,13 @@
+//
+//  CMFriRemindView.h
+//  ConstellationMaster_iOS
+//
+//  Created by qujiahong on 2018/1/31.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMFriRemindView : UIView
+
+@end
---
+++ ConstellationMaster_iOS/ConstellationMaster_iOS/Classes/Friends好友/View/CMFriRemindView.m
@@ -0,0 +1,21 @@
+//
+//  CMFriRemindView.m
+//  ConstellationMaster_iOS
+//
+//  Created by qujiahong on 2018/1/31.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import "CMFriRemindView.h"
+
+@implementation CMFriRemindView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
